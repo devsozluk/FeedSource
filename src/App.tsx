4c666fb7122912913ea2d35a14d476de7706e68e
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2023-04-21 01:36:43 +0300

    chore: eslint added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
+
 function App() {
+  const [state, setState] = useState(0);
+
   return (
     <div className="font-sans w-full h-screen space-y-2 flex flex-col items-center justify-center">
       <div className="text-3xl font-semibold">Inited FeedSource</div>
-      <div className="text-lg font-normal text-zinc-500">Open Source Feedback React Package</div>
+      <div className="text-lg font-normal text-zinc-500">
+        Open Source Feedback React Package
+      </div>
     </div>
   );
 }
